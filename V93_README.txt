f09300e7b6c1b70c5415cb5ab984eb5b1724c102
mjrosati <mattjrosati@gmail.com>
2026-09-02 18:29:56 -0500

    Add files via upload

---
+++ V93_README.txt
@@ -0,0 +1,7 @@
+Coach Lineup v93
+- Same player box size and field layout as v92.
+- Position text increased again.
+- Jersey number text increased again.
+- NEXT highlights unchanged.
+- Bottom game controls unchanged.
+- No database changes.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="coach-lineup-v92-larger-player-font";
+const CACHE="coach-lineup-v93-extra-large-player-font";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest",
   "./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
