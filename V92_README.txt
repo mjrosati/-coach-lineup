e04c597859aba2e39083945a8416c5eeb5b29a9f
mjrosati <mattjrosati@gmail.com>
2026-09-02 18:19:20 -0500

    Add files via upload

---
+++ V92_README.txt
@@ -0,0 +1,7 @@
+Coach Lineup v92
+- Keeps v91 clean tablet fullscreen layout.
+- Player boxes stay the same size.
+- Position font increased.
+- Jersey number font increased.
+- Bottom game controls unchanged.
+- No database changes.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="coach-lineup-v91-clean-tablet";
+const CACHE="coach-lineup-v92-larger-player-font";
 const CORE=[
   "./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest",
   "./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
